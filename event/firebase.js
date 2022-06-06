541dbd21e0af5433f1dc3af873fcb3bf6331c1c6
angky-zenius <angky.putra@zeniuseducation.com>
2022-06-06 15:13:50 +0700

    Add firebase client

--- event/firebase.js
+++ event/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
+const { initializeApp } = require("firebase/app");
+const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,8 +19,32 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+/*
+export declare interface User extends UserInfo {
+  readonly emailVerified: boolean;
+  readonly isAnonymous: boolean;
+  readonly metadata: UserMetadata;
+  readonly providerData: UserInfo[];
+  readonly refreshToken: string;
+  readonly tenantId: string | null;
+  getIdToken(forceRefresh?: boolean): Promise<string>;
+  getIdTokenResult(forceRefresh?: boolean): Promise<IdTokenResult>;
+}
+*/
 
 function testLogin() {
-    
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, "angky.putra@zeniuseducation.com", "p4ssw0rd")
+    .then((userCredential) => {
+      const emailVerified = userCredential.user.emailVerified;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode + ": " + errorMessage);
+    });
 }
+
+testLogin();
